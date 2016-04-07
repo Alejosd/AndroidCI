e75f030306b9d696fd3245aee0ac822f8f535761
Alejosd <alejosd5@gmail.com>
2016-04-07 20:04:35 -0300

    build.gradle configuracion para jacoco cobertura de codigo

---
+++ app/src/test/java/alejosd5/tumblr/com/sd/Ejemplo.java
@@ -0,0 +1,20 @@
+package alejosd5.tumblr.com.sd;
+
+/**
+ * Created by alejosd on 22/02/2016.
+ */
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class Ejemplo {
+
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertEquals(4, 2 + 2);
+    }
+}
