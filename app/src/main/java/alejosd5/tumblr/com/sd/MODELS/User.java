5c39deea99b8c28be42f1f6c6a496033323866ed
Alejosd <alejosd5@gmail.com>
2016-04-24 10:46:09 -0300

    agregado modelo user version.properties gitignore

---
+++ app/src/main/java/alejosd5/tumblr/com/sd/MODELS/User.java
@@ -0,0 +1,18 @@
+package alejosd5.tumblr.com.sd.models;
+
+public class User {
+
+    private String username;
+
+    public  User(String username){
+        this.username=username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
